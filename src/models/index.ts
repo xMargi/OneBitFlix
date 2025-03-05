e397ddd1817dd2aa8967c34a7ebeb1e1c02b6ff8
Vini <viniciusff200@gmail.com>
2025-03-05 16:11:27 -0300

    Adicionando a tabela de cursos e adicionando o relacionamento entre ela e a tabela de categorias

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,12 @@
 import { Category } from "./Category";
+import { Course } from "./Course";
+
+//relacionamento entre a tabela Course e a Category
+Category.hasMany(Course)
+
+Course.belongsTo(Category)
 
 export {
-    Category
+    Category,
+    Course
 }
