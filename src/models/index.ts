369f870f8e2dd9592ca230df7899f00ded14b4bb
Vini <viniciusff200@gmail.com>
2025-03-07 19:46:02 -0300

    Criando a tabela de episodios e adicionando ela ao catálogo do adminJs

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,17 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
+import { Episode } from "./Episode";
 
 //relacionamento entre a tabela Course e a Category
 Category.hasMany(Course)
-
 Course.belongsTo(Category)
+
+//relacionamento tabela de episodios com os cursos 1:n
+Course.hasMany(Episode)
+Episode.belongsTo(Course)
 
 export {
     Category,
-    Course
+    Course,
+    Episode
 }
