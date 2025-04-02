dd47c3524f787d6ff945418601e1b38f9cc686d7
Vinicius <viniciusff200@gmail.com>
2025-04-02 16:31:01 -0300

    adicionando o cors

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import express from 'express'
 import { database } from './database'
 import { adminJs, adminJsRouter } from './adminJs'
 import { router } from './routes'
+import cors from 'cors'
 
 
 const app = express()
-
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.json())
 
